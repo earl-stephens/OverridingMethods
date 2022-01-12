b75f04301a12362180166c1e6f39245c3b683da6
earl-stephens <stephensearl@hotmail.com>
2022-01-11 22:58:12 -0700

    Demonstrate overriding methods

---
+++ src/application/Cat.java
@@ -0,0 +1,13 @@
+package application;
+
+public class Cat {
+
+	//void vocalize() is the signature of the method
+	public void vocalize() {
+		System.out.println("Roar!");
+	}
+	
+	public void hunt() {
+		System.out.println("Hunting!");
+	}
+}
